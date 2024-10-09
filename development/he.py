077048b84f2a77e5ceaddc49af60d08c2d169260
chataignault <leonchat@orange.fr>
2024-10-09 20:41:49 +0100

    add heisenberg group development

---
+++ development/he.py
@@ -0,0 +1,41 @@
+"""
+Adapted from https://github.com/Lezcano/geotorch/blob/master/geotorch/so.py
+"""
+import torch
+from torch import nn
+
+
+class he(nn.Module):
+    def __init__(self, size):
+        """
+        Heisenberg(n) lie algebra matrices, parametrized in terms of
+        by a general linear matrix with shape (...,...,n,n).
+        Args:
+            size (torch.size): Size of the tensor to be parametrized
+        """
+        super().__init__()
+        self.size = size
+
+    @staticmethod
+    def frame(X: torch.tensor) -> torch.tensor:
+        """ parametrise Heisenberg lie algebra from the general linear matrix X
+
+        Args:
+            X (torch.tensor): (...,n,n)
+        
+
+        Returns:
+            torch.tensor: (...,n,n)
+        """
+
+        X = X.tril(-1)
+
+        return X
+
+    def forward(self, X: torch.tensor) -> torch.tensor:
+        if len(X.size()) < 2:
+            raise ValueError('weights has dimension < 2')
+        if X.size(-2) != X.size(-1):
+            raise ValueError('not sqaured matrix')
+        return self.frame(X)
+
